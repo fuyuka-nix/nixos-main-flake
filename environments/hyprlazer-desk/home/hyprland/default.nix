88b551812bc72d9b772878c61d9094609132ac79
Fuyuka.nix <fuyuka.nix@proton.me>
2026-01-17 19:15:04 -0600

    removed hyprland from hyprland configuration

--- environments/hyprlazer-desk/home/hyprland/default.nix
+++ environments/hyprlazer-desk/home/hyprland/default.nix
@@ -8,10 +8,6 @@ let
   palette = config.lib.stylix.colors;
 in
 {
-  imports = [
-    ./binds.nix
-  ];
-
   services = {
     hypridle.enable = true;
     hyprpaper.enable = true;
@@ -22,28 +18,6 @@ in
   };
 
   stylix.targets.hyprland.enable = false;
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = true;
-    xwayland.enable = true;
-    extraConfig = builtins.readFile ./hyprland.conf;
-    submaps = {
-      "Screenshot".settings = {
-        bind = [
-          ", PRINT, exec, hyprshot -m region -z --clipboard-only ; hyprctl dispatch submap reset"
-          ", escape, submap, reset"
-        ];
-      };
-    };
-    settings = {
-      "$terminal" = "kitty";
-
-      general = lib.mkForce {
-        "col.inactive_border" = "rgb(${palette.base00})";
-        "col.active_border" = "rgb(${palette.base04})";
-      };
-    };
-  };
 
   home.packages = with pkgs; [
     wofi
