76797df4b07ab32b996a314e48ee46f549c62c83
Fuyuka.nix <fuyuka.nix@proton.me>
2026-04-26 20:47:20 -0600

    extended zsh aliases

--- users/frozenfox/default.nix
+++ users/frozenfox/default.nix
@@ -31,9 +31,17 @@
     heroic
   ];
 
-  programs.zsh.shellAliases = {
-    nixos-rsrf = "sudo nixos-rebuild switch --flake ~/mysystem/#pavillion";
-    nixos-rbrf = "sudo nixos-rebuild boot --flake ~/mysystem/#pavillion";
-    home-srf = "home-manager switch --flake ~/mysystem/#frozenfox";
+  programs.zsh.shellAliases =
+  let
+    rf = "~/mysystem/#pavillion";
+  in
+  {
+    nixos-rsrf = "sudo nixos-rebuild switch --flake ${rf}";
+    nixos-rbrf = "sudo nixos-rebuild boot --flake ${rf}";
+    nixos-rdrrf = "sudo nixos-rebuild dry-run --flake ${rf}";
+    home-msrf = "home-manager switch --flake ${rf}";
+    nix-home-rsrf = "sudo nixos-rebuild switch --flake ${rf}; home-manager switch --flake ${rf}";
+    kitty-d1 = "kitty --detach -1";
+    kitty-d = "kitty --detach";
   };
 }
