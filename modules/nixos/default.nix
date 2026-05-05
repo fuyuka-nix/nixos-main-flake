4445d3f4f6a6c81ca9b3601ca36f40080080f8f9
Fuyuka.nix <fuyuka.nix@proton.me>
2026-05-05 14:53:54 -0600

    adjusted default nixos module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,39 +1,45 @@
 lib:
-{
-  nixvim-profiles = {
-    frosted-editor = ./nixvim/frosted-editor;
-  };
-  starship = ./starship;
-  default = {
-    boot = {
-      loader = {
-        systemd-boot.enable = true;
-        systemd-boot.editor = false;
-        efi.canTouchEfiVariables = true;
-        timeout = 25;
-      };
-    };
-
-    nix = {
-      settings.auto-optimise-store = true;
-      gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 7d";
-      };
-      extraOptions = ''
-        experimental-features = nix-command flakes
-        keep-outputs = true
-        keep-derivations = true
-      '';
-    };
-  };
-}
-// (lib.genAttrs [
+(lib.genAttrs [
   "fonts"
   "locale-es-cr"
   "pipewire"
   "steam"
   "arrpc"
 ] (moduleName: ./${moduleName}.nix))
+// {
+  starship = ./starship;
+  default = {
+    options = {
+      modules.sysPath = lib.mkOption {
+	default = "/etc/nixos";
+	type = lib.types.str;
+	description = "Where the flake is located in the filesystem";
+      };
+    };
+    config = {
+      boot = {
+	loader = {
+	  systemd-boot.enable = true;
+	  systemd-boot.editor = false;
+	  efi.canTouchEfiVariables = true;
+	  timeout = 25;
+	};
+      };
+
+      nix = {
+	settings.auto-optimise-store = true;
+	gc = {
+	  automatic = true;
+	  dates = "weekly";
+	  options = "--delete-older-than 7d";
+	};
+	extraOptions = ''
+	  experimental-features = nix-command flakes
+	  keep-outputs = true
+	  keep-derivations = true
+	'';
+      };
+    };
+  };
+}
 
