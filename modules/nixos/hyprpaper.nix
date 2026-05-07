48dfca49fd27c1192dfeaa9ca3ab1810a22265a7
Fuyuka.nix <fuyuka.nix@proton.me>
2026-05-07 13:51:14 -0600

    removed two nonexistent settings at hyprpaper nixos module (oopsx2)

--- modules/nixos/hyprpaper.nix
+++ modules/nixos/hyprpaper.nix
@@ -6,8 +6,6 @@
   systemd.user.services.hyprpaper = {
     name = "hyprpaper.service";
     description = "hyprpaper (duh)";
-    after = [ config.wayland.systemd.target ];
-    partOf = [ config.wayland.systemd.target ];
     wantedBy = [ "graphical-session.target" ];
     path = with pkgs; [
       hyprland
