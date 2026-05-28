bc789701d8945f34ec6836a14198bcdbaa0818de
Fuyuka.nix <fuyuka.nix@proton.me>
2026-05-27 18:12:35 -0600

    adjusted networking settings of host pavillion

--- hosts/pavillion/networking.nix
+++ hosts/pavillion/networking.nix
@@ -22,5 +22,16 @@
 	role = "relay";
       };
     };
+    yggdrasil = {
+      enable = true;
+      configFile = "/etc/yggdrasil.conf";
+      group = "wheel";
+      settings = {
+	Peers = [
+	  "tcp://satori.nadeko.net:44441"
+	  "tcp://ygg-1.okade.pro:20000"
+	];
+      };
+    };
   };
 }
