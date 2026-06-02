b54d80443b4297b21de623fee0de21bcb928b71f
fuyuka-nix <fuyuka.nix@proton.me>
2026-06-02 15:58:50 -0600

    added yggdrasil settings back

--- hosts/pavillion/networking.nix
+++ hosts/pavillion/networking.nix
@@ -1,6 +1,9 @@
 {
   ...
 }:
+let
+  yggPrefix = "300:9eba:ceda:4c59";
+in
 {
   networking = {
     networkmanager.enable = true;
@@ -17,12 +20,24 @@
 	enable = true;
       };
     };
-    /*
     yggdrasil = {
       enable = true;
-      configFile = "/etc/yggdrasil.conf";
       group = "wheel";
+      persistentKeys = true;
+      openMulticastPort = true;
+      settings = {
+	IfName = "ygg0";
+	Listen = [ "[::]:0" ];
+	Peers = [
+	  "tcp://satori.nadeko.net:44441"
+	  "tcp://ygg.nadeko.net:44441"
+	  "tcp://ygg-1.okade.pro:20000"
+	];
+	MulticastInterfaces = [ "eno1" "wlo1" ];
+	AllowedPublicKeys = [
+	  "466a6c8313ae63052639a09bc053f1be9901357633add9ad8de636dfeb604a6f"
+	];
+      };
     };
-    */
   };
 }
