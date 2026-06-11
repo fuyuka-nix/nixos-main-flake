17ac267602570de4cb6dfabeaf12939f4467b4f1
fuyuka-nix <fuyuka.nix@proton.me>
2026-06-11 13:58:27 -0600

    renamed ./hosts to ./modules/hosts accordingly to dendritic pattern

--- flake.nix
+++ flake.nix
@@ -1,17 +1,44 @@
 {
   inputs = {
-    fuyupkgs = {
-      type = "path";
-      path = "./packages";
+    nixpkgs = {
+      type = "github";
+      owner = "nixos";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+    affinity-nix = {
+      type = "github";
+      owner = "mrshmllow";
+      repo = "affinity-nix";
+    };
+    sops-nix = {
+      type = "github";
+      owner = "Mic92";
+      repo = "sops-nix";
+    };
+    custompkgs = {
+      type = "github";
+      owner = "rishabh5321";
+      repo = "custom-packages-flake";
     };
   };
 
-  outputs =
-    {
-      fuyupkgs,
-      ...
-    }:
-    {
-      nixosConfigurations = import ./hosts fuyupkgs;
+  outputs = inputs:
+  let
+    pkgs = import inputs.nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
     };
+  in
+  {
+    nixosConfigurations = {
+      pavillion = inputs.nixpkgs.lib.nixosSystem {
+	inherit pkgs;
+	modules = [
+	  ./modules/hosts/pavillion
+	  ./modules/hosts/pavillion/hardware-configuration.nix
+	];
+      };
+    };
+  };
 }
