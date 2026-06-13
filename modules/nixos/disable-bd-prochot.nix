687d2ff7589b54a5d1c1765d78f8f8469b8fc9b2
fuyuka-nix <fuyuka.nix@proton.me>
2026-06-12 18:05:15 -0600

    fixed disable-bd-prochot module

--- modules/nixos/disable-bd-prochot.nix
+++ modules/nixos/disable-bd-prochot.nix
@@ -8,7 +8,7 @@ let
     set -e
     modprobe msr 2>/dev/null || true
     for cpu in /dev/cpu/[0-9]*; do
-	cpu_id="$\{cpu##*/cpu/\}"; cpu_id="$\{cpu_id%%/*\}"
+	cpu_id="''${cpu##*/cpu/}"; cpu_id="''${cpu_id%%/*}"
 	cur=$(rdmsr -p "$cpu_id" 0x1FC)
 	new=$(( 16#$cur & ~1 ))
 	wrmsr -p "$cpu_id" 0x1FC "$(printf '0x%x' "$new")"
