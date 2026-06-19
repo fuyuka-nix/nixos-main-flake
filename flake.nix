221b193570181491f74d429bac1bec71119fbdf5
Fuyuka.nix <fuyuka.nix@proton.me>
2026-06-18 22:02:38 -0600

    fixed typo

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     den.url = "github:denful/den";
     preservation.url = "github:nix-community/preservation";
     disko.url = "github:nix-community/disko";
-    disko.inpurts.nixpkgs.follows = "nixpkgs";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
     affinity-nix.url = "github:mrshmllow/affinity-nix";
     custompkgs.url = "github:rishabh5321/custom-packages-flake";
