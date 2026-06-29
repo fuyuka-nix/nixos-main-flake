8e1cd4277df37a8c0a87631b6451a0aaf18aa510
Fuyuka.nix <fuyuka.nix@proton.me>
2026-06-28 20:27:03 -0600

    updated osu-resources package

--- _packages/osu-resources.nix
+++ _packages/osu-resources.nix
@@ -6,28 +6,39 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "osu-resources";
-  version = "d315afeb0edae1c320e10506af0b5b1da18256a6";
+  version = "2026.615.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "ppy";
     repo = "osu-resources";
-    rev = version;
-    sha256 = "sha256-pMBrrE8ua5CAAHL1X+dIvDCln6Ut8ZzfUgkUrnxC+6I=";
+    tag = version;
+    hash = "sha256-V3MYeJm8Ypd8d2cVbKn4DcHgsDcJoH/7+RAWxhT3VFk=";
   };
 
   dontStrip = true;
 
   installPhase = ''
-    mkdir -p $out
-    cp -r osu.Game.Resources/Textures $out/
-    cp -r osu.Game.Resources/Samples $out/
+    runHook preInstall
+
+    assetsOut="$out/share/${pname}"
+    mkdir -p $assetsOut
+
+    cp -r osu.Game.Resources/Textures $assetsOut/
+    cp -r osu.Game.Resources/Samples $assetsOut/
+    cp -r osu.Game.Resources/Tracks $assetsOut/
+    cp -r osu.Game.Resources/Fonts $assetsOut/
+    cp -r osu.Game.Resources/Localisation $assetsOut/
+    cp -r osu.Game.Resources/Shaders $assetsOut/
+    cp -r osu.Game.Resources/Skins $assetsOut/
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "Icons and Samples from osu!resources repo";
     homepage = "https://github.com/ppy/osu-resources";
     license = licenses.cc-by-nc-40;
-    maintainers = with maintainers; [ Fuyuka-nix ];
+    maintainers = with maintainers; [ fuyuka-nix ];
     platforms = platforms.all;
   };
 }
