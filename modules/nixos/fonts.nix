f0b484a2e527b62405b988e88297ed07f7c94c3e
Fuyuka.nix <fuyuka_nix@disroot.org>
2026-08-25 08:16:35 -0600

    fixed indentation for random modules

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -6,24 +6,24 @@
   den.aspects.fonts.nixos = { pkgs, ... }: {
     fonts = {
       packages = with pkgs; [
-	nerd-fonts.hack
-	# jetbrains-mono
-	# openmoji-color
-	noto-fonts-color-emoji
-	# nerd-fonts.dejavu-sans-mono
-	# nerd-fonts.noto
-	libre-caslon
-	babelstone-han
+        nerd-fonts.hack
+        # jetbrains-mono
+        # openmoji-color
+        noto-fonts-color-emoji
+        # nerd-fonts.dejavu-sans-mono
+        # nerd-fonts.noto
+        libre-caslon
+        babelstone-han
       ];
 
       fontconfig = {
-	hinting.autohint = true;
-	# defaultFonts = {
-	#   sansSerif = [ "Noto Sans" "Hack Nerd Font" ];
-	#   serif = [ "Libre Caslon Text" ];
-	#   monospace = [ "Hack Nerd Font Mono" ];
-	#   emoji = [ "OpenMoji Color" ];
-	# };
+        hinting.autohint = true;
+        # defaultFonts = {
+        #   sansSerif = [ "Noto Sans" "Hack Nerd Font" ];
+        #   serif = [ "Libre Caslon Text" ];
+        #   monospace = [ "Hack Nerd Font Mono" ];
+        #   emoji = [ "OpenMoji Color" ];
+        # };
       };
     };
   };
--- modules/nixos/locale-es-cr.nix
+++ modules/nixos/locale-es-cr.nix
@@ -8,15 +8,15 @@
     i18n = {
       defaultLocale = "es_MX.UTF-8";
       extraLocaleSettings = {
-	LC_ADDRESS = "es_CR.UTF-8";
-	LC_IDENTIFICATION = "es_CR.UTF-8";
-	LC_MEASUREMENT = "es_CR.UTF-8";
-	LC_MONETARY = "es_CR.UTF-8";
-	LC_NAME = "es_CR.UTF-8";
-	LC_NUMERIC = "es_CR.UTF-8";
-	LC_PAPER = "es_CR.UTF-8";
-	LC_TELEPHONE = "es_CR.UTF-8";
-	LC_TIME = "es_CR.UTF-8";
+        LC_ADDRESS = "es_CR.UTF-8";
+        LC_IDENTIFICATION = "es_CR.UTF-8";
+        LC_MEASUREMENT = "es_CR.UTF-8";
+        LC_MONETARY = "es_CR.UTF-8";
+        LC_NAME = "es_CR.UTF-8";
+        LC_NUMERIC = "es_CR.UTF-8";
+        LC_PAPER = "es_CR.UTF-8";
+        LC_TELEPHONE = "es_CR.UTF-8";
+        LC_TIME = "es_CR.UTF-8";
       };
     };
 
